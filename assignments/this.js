ad510c4e9bdeaa7e1a30ec3d0c855dcbb13388bc
Rae Glazier <glazier.home.life@gmail.com>
2019-10-17 15:00:14 -0700

    completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,51 @@
 // Principle 1
 
 // code example for Window Binding
+function ghost(){
+    console.log(this.boo);
+}
+
+var boo = '👻 booooo';
+ghost();
 
 // Principle 2
 
 // code example for Implicit Binding
+let person = {
+    name: 'Sorcha',
+    age: 150,
+    location: 'The Shadow Nexus',
+    greet: function(){
+        console.log(`Hello, my name is ${this.name}, I'm from ${this.location} and I'm ${this.age} years old`)
+    }
+}
+person.greet();
 
 // Principle 3
 
 // code example for New Binding
+function food(favorite){
+    this.food = favorite;
+}
+let myFavoriteFood = new food('Spaghetti');
+
+console.log(`My favorite food is ${myFavoriteFood.food}`);
 
 // Principle 4
 
 // code example for Explicit Binding
+function hero(){
+    console.log(this.saying);
+}
+
+let origHero = {
+    name: 'Cairn',
+    saying: 'For my tribe',
+}
+
+let newHero = {
+    name: 'Sorcha',
+    saying: 'For my children',
+}
+hero.call(origHero);
+hero.call(newHero);
