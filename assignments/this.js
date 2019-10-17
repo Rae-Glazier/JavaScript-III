a2841faa0d1afe03566b9246bfa1faf576a3f8a4
Rae Glazier <glazier.home.life@gmail.com>
2019-10-17 14:41:38 -0700

    completed first part of this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - this rule applies when none of the other this keyword rules applies, unless strict mode is being used
+* 2. Implicit Binding - this is the most common used rule. it knows what to bind the information to by looking to the left of the dot. 
+* 3. Explicit Binding - call, apply & bind are used to explicityly state what the this keyword is 
+* 4. New Binding - constructs a new object and using the new binding points to it
 *
 * write out a code example of each explanation above
 */
